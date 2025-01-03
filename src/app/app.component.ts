e2f96a73deef89b0b42d520450debc98b4bfedc5
AkankshaChavan <git@stackblitz.com>
2025-01-03 11:00:20 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ButtonModule],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  title = 'my-app';
+}
